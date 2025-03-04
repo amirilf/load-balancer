bbe90d84d8763aaec492603076f2312b17da2e1c
Amir Mohammad Khorasani <amirilf@protonmail.com>
2025-03-04 11:23:00 +0330

    Init

---
+++ src/main/java/com/github/amirilf/load_balancer/Application.java
@@ -0,0 +1,13 @@
+package com.github.amirilf.load_balancer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LoadBalancerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/amirilf/load_balancer/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.amirilf.load_balancer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
